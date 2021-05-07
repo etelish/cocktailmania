0c7277efb3e46d3173d7f8f7b3a42e3466041728
Eithel Anderson <etelisha@googlemail.com>
2021-05-07 13:12:18 +0100

    fix: amended env variables to include next_public

--- pages/_app.js
+++ pages/_app.js
@@ -21,13 +21,12 @@ function onAuthStateChange(callback) {
 function MyApp({ Component, pageProps }) {
     const firebaseConfig = {
         apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     };
-    console.log(firebaseConfig);
     if (!firebase.apps.length) {
         firebase.initializeApp(firebaseConfig);
     } else {
