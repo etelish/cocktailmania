37d2cff14158a57d16633389c161a4be5159c39c
Eithel Anderson <etelisha@googlemail.com>
2021-05-18 17:18:46 +0100

    feat: created fave cocktail button component to write to db

---
+++ components/FaveButton.js
@@ -0,0 +1,33 @@
+import { React } from 'react';
+import 'firebase/firestore';
+import firebase from 'firebase';
+
+function FaveButton({ userId, cocktailId }) {
+    const db = firebase.firestore();
+
+    const saveCocktail = () => {
+        try {
+            db.collection('Cocktails')
+                .doc(userId)
+                .set(
+                    {
+                        faveCocktails:
+                            firebase.firestore.FieldValue.arrayUnion(
+                                cocktailId,
+                            ),
+                    },
+                    { merge: true },
+                );
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <button type="submit" onClick={saveCocktail}>
+            Add to fave list
+        </button>
+    );
+}
+
+export default FaveButton;
--- pages/favouriteCocktails.js
+++ pages/favouriteCocktails.js
@@ -1,5 +1,7 @@
 import { React, useContext, useEffect } from 'react';
 import 'firebase/auth';
+import firebase from 'firebase';
+import 'firebase/firestore';
 import { useRouter } from 'next/router';
 
 import { UserContext } from '../lib/user-context';
@@ -8,15 +10,30 @@ function FaveCocktails() {
     const router = useRouter();
     const user = useContext(UserContext);
 
+    const db = firebase.firestore();
+    const docRef = db.collection('Cocktails').doc(user.userId);
+
     useEffect(() => {
         if (!user?.loggedIn && user !== undefined) {
             router.push('/login');
         }
+        docRef
+            .get()
+            .then((doc) => {
+                if (doc.exists) {
+                    console.log('Document data:', doc.data());
+                } else {
+                    console.log('No such document!');
+                }
+            })
+            .catch((error) => {
+                console.log('Error getting document:', error);
+            });
     }, [user, router]);
 
     return (
         <div>
-            <h1>fave cocktail</h1>
+            <h1>hi</h1>
         </div>
     );
 }
